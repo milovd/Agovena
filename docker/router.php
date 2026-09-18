224053ffc03aba13e5f7339dc343d427d6ac46e0
milovd <175127637+milovd@users.noreply.github.com>
2026-09-18 15:54:22 +0200

    fix: serve Docker static assets correctly

---
+++ docker/router.php
@@ -0,0 +1,17 @@
+<?php
+
+$publicRoot = __DIR__.'/../public';
+$requestPath = parse_url($_SERVER['REQUEST_URI'] ?? '/', PHP_URL_PATH);
+$requestPath = is_string($requestPath) ? rawurldecode($requestPath) : '/';
+$relativePath = ltrim(str_replace('\\', '/', $requestPath), '/');
+
+if (
+    $relativePath !== ''
+    && !str_contains($relativePath, "\0")
+    && !preg_match('#(?:^|/)\.\.(?:/|$)#', $relativePath)
+    && is_file($publicRoot.'/'.str_replace('/', DIRECTORY_SEPARATOR, $relativePath))
+) {
+    return false;
+}
+
+require $publicRoot.'/index.php';
